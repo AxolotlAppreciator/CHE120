9531fd35515d00c5134229b18a80ddd994eac552
AxolotlAppreciator <107078602+AxolotlAppreciator@users.noreply.github.com>
2024-11-27 17:08:04 -0500

    Update master.py

--- master.py
+++ master.py
@@ -1,1 +1,17 @@
+import pygame
+import time
 #This is where main stuff will go (game process). Frfr. Frfr.
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(30)
+
+pygame.quit()
